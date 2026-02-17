560f487c0a43439b8986f410526d720b7e9c276d
andrei neagoe <daria_maria.neagoe@stud.acs.upb.ro>
2026-02-17 14:55:42 +0200

    Code for opening camera

--- camera.py
+++ camera.py
@@ -1,1 +1,8 @@
 import cv2
+cam = cv2.VideoCapture(0)
+while cam.isOpened():
+    ret, frame = cam.read()
+    cv2.imshow("new", frame)
+    if cv2.waitKey(1) == ord('q'):
+        break
+cv2.destroyAllWindows()

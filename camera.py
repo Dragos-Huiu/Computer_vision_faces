1055698ce8fa71fa9d8ed1c8ffd9e63ebb4627ba
andrei neagoe <daria_maria.neagoe@stud.acs.upb.ro>
2026-02-18 14:33:35 +0200

    Add screenshot functionality

--- camera.py
+++ camera.py
@@ -1,10 +1,16 @@
 import cv2
+import os
 
 def operate_camera(index):
     cam = cv2.VideoCapture(index)
+    os.makedirs(f"camera{index}", exist_ok = True)
+    n = 0
     while cam.isOpened():
         ret, frame = cam.read()
         if cv2.waitKey(1) == ord('q') or ret == 0:
             break
         cv2.imshow(f"Camera {index}", frame)
+        if cv2.waitKey(1) == ord('s'):
+            n += 1
+            cv2.imwrite(f"camera{index}\\img{n}.png", frame)
     cv2.destroyAllWindows()
